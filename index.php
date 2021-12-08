7ce9ab28cda424af59a14037574564f661d58f11
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-09 00:06:45 +0700

    cek semalem

--- index.php
+++ index.php
@@ -1,213 +1,78 @@
 <?php 
-    $content = file_get_contents("php://input");
-    if ($content) {
-        $token = '5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo';
-        
-        $apiLink = "https://api.telegram.org/bot$token/";  
-
-        echo '<pre>content = '; print_r($content); echo '</pre>';
-        $update = json_decode($content, true);
-        if(!@$update["message"]) $val = $update['callback_query'];
-        else $val = $update;
-
-        $chat_id = $val['message']['chat']['id'];
-        $text = $val['message']['text'];
-        $update_id = $val['update_id'];
-        $sender = $val['message']['from'];
-    }
-?>
-
-<?php 
     $sumber = "https://indodax.com/api/tickers";
     $konten = file_get_contents($sumber);
     $data = json_decode($konten, true);
     $data = $data['tickers'];
-    $a = 1;
-    $panjangData = count($data);
-
-    if ($text=="/view_indodax") {
-        $msg .= "Bot Developed by Kelompok API 7!%0a%0a%0a";
-        for ($i=0; $i < ($panjangData*1/8); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg1 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*1/8))+1 ; $i < floor(($panjangData*2/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg2 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*2/8))+1 ; $i < floor(($panjangData*3/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg3 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*3/8))+1 ; $i < floor(($panjangData*4/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg4 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*4/8))+1 ; $i < floor(($panjangData*5/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg5 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*5/8))+1 ; $i < floor(($panjangData*6/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg6 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*6/8))+1 ; $i < floor(($panjangData*7/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg7 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        for ($i= floor(($panjangData*7/8))+1 ; $i < floor(($panjangData*8/8)); $i++) { 
-            $nomor = $i+1;
-            $asset = array_keys($data)[$i];
-            $last = $data[$asset]['last'];
-            $high = $data[$asset]['high'];
-            $low = $data[$asset]['low'];
-            $sell = $data[$asset]['sell'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $high) {
-                $status = "Harga Tertinggi !!!";
-            }elseif($last == $low){
-                $status = "Harga terendah !!!";
-            }else{
-                $status = "Harga diantara !!!";
-            }
-            $msg8 .= "Nomor : ".$nomor."%0aAsset : ".$asset."%0aLast Price : ".$last."%0aHigh 24H : ".$high."%0aLow 24H : ".$low."%0aSell : ".$sell."%0aBuy : ".$buy."%0a".$status."%0a%0a";
-        }
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg1."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg2."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg3."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg4."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg5."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg6."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg7."...");
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msg.$msg8."...");
-    }
-    if ($text=="/low_indodax") {
-        $msgStatusCoinLow = "Coin yang mengalami harga terendah : %0a";
-        $coinLowPrice = array();
-        $nomor = 0;
-        for ($i=0; $i < $panjangData; $i++) { 
-            $asset = array_keys($data)[$i];
-            $low = $data[$asset]['low'];
-            $last = $data[$asset]['last'];
-            $buy = $data[$asset]['buy'];
-            if ($last == $low) {
-                $nomor = $nomor+1;
-                $msgLow .= $nomor.".".$asset."%0a"."Harga Beli : ".$low."%0a";
-            }
-        }
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msgStatusCoinLow.$msgLow."...");
-    }
-    if ($text=="/start") {
-        $msgWelcome = "Selamat datang di bot Monitoring Harga Cryptocurrency, kamu dapat menyesuaikan command yang tersedia sesuai dengan kebutuhan kamu :)";
-
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msgWelcome."...");
-    }
-    if ($text=="/high_indodax") {
-        $msgStatusCoinLow = "Coin yang mengalami harga tertinggi : %0a";
-        $coinLowPrice = array();
-        $nomor = 0;
-        for ($i=0; $i < $panjangData; $i++) { 
-            $asset = array_keys($data)[$i];
-            $high = $data[$asset]['high'];
-            $last = $data[$asset]['last'];
-            $sell = $data[$asset]['sell'];
-            if ($last == $high) {  
-                $nomor = $nomor+1;
-                $msgHigh .= $nomor.".".$asset."%0a"."Harga Jual : ".$sell."%0a";
-            }
-        }
-        file_get_contents($apiLink . "sendmessage?chat_id=$chat_id&text=".$msgStatusCoinLow.$msgHigh."...");
-    }
-    else echo 'Only telegram can access this url.';
 ?>
+
+<?php 
+    $arrayChatId = ['1062055729','1536466209','1087365447'];
+
+    function sendRecomendtoBuy($msgRecomendtoBuy,$namaCoin,$low24H,$buyPrice){
+        // tinggal tambahin for
+        global $arrayChatId;
+        for ($i=0; $i < count($arrayChatId); $i++) { 
+            $msgRecomendtoBuy = "Recomend to Buy :%0a".$namaCoin."%0aLow 24H : ".$low24H."%0aBuy Price : ".$buyPrice;
+            $api = "https://api.telegram.org/bot5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo/sendmessage?chat_id=$arrayChatId[$i]&text=$msgRecomendtoBuy";
+            $konten = file_get_contents($api);
+            $data = json_decode($konten, true);
+        }
+    }
+
+    function sendRecomendtoSell($msgRecomendtoSell,$namaCoin,$high24H,$sellPrice){
+        global $arrayChatId;
+        for ($i=0; $i < count($arrayChatId) ; $i++) { 
+            $msgRecomendtoSell = "Recomend to Sell :%0a".$namaCoin."%0aHigh 24H : ".$high24H."%0aSell Price : ".$sellPrice;
+            $api = "https://api.telegram.org/bot5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo/sendmessage?chat_id=$arrayChatId[$i]&text=$msgRecomendtoSell";
+            $konten = file_get_contents($api);
+            $data = json_decode($konten, true);
+        }
+    }
+?>
+
+<?php 
+    header( "refresh:30" );
+?>
+
+<?php 
+    foreach ($data as $row => $value) {
+        $msgRecomendtoSell = "";
+        if ($value['high'] - $value['last'] == 0) {
+            sendRecomendtoSell($msgRecomendtoSell,$row,$value['high'],$value['sell']);
+        }
+        if ($value['last'] - $value['low'] == 0) {
+            SendRecomendtoBuy($msgRecomendtoSell,$row,$value['high'],$value['sell']);
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <div id="Timer"></div>
+        <!-- coba lihat -->
+
+        <script>
+            var timeLeft = 10;
+            var elem = document.getElementById("Timer");
+
+            var timerId = setInterval(countdown, 1000);
+
+            function countdown() {
+                if (timeLeft == 0) {
+                    clearTimeout(timerId);
+                } else {
+                    elem.innerHTML = timeLeft + " seconds remaining";
+                    timeLeft--;
+                }
+            }
+        </script>
+    </body>
+</html>
