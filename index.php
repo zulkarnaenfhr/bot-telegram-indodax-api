fa5ed59fb531e97d6db038707099cfc3af1ecbf3
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-09 23:25:36 +0700

    cobain

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
         // tinggal tambahin for
         global $arrayChatId;
         for ($i=0; $i < count($arrayChatId); $i++) { 
-            // $msgRecomendtoBuy = "Recomend to Buy :%0a".$namaCoin."%0aLast Price : Rp ".number_format($lastPrice)."%0aLow 24H : Rp ".number_format($low24H)."%0aBuy Price : Rp ".number_format($buyPrice);
-            $msgRecomendtoBuy = "Recomend to Buy :%0a".$namaCoin."%0aLow 24H : Rp ".number_format($low24H)."%0aBuy Price : Rp ".number_format($buyPrice);
+            $msgRecomendtoBuy = "Recomend to Buy :%0a".$namaCoin."%0aLast Price : Rp ".number_format($lastPrice)."%0aLow 24H : Rp ".number_format($low24H)."%0aBuy Price : Rp ".number_format($buyPrice);
             $api = "https://api.telegram.org/bot5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo/sendmessage?chat_id=$arrayChatId[$i]&text=$msgRecomendtoBuy";
             $konten = file_get_contents($api);
             // $data = json_decode($konten, true);
@@ -23,8 +22,7 @@
     function sendRecomendtoSell($msgRecomendtoSell,$namaCoin,$lastPrice,$high24H,$sellPrice){
         global $arrayChatId;
         for ($i=0; $i < count($arrayChatId) ; $i++) { 
-            // $msgRecomendtoSell = "Recomend to Sell :%0a".$namaCoin."%0aLast Price : Rp ".number_format($lastPrice)."%0aHigh 24H : Rp ".number_format($high24H)."%0aSell Price : Rp ". number_format($sellPrice);
-            $msgRecomendtoSell = "Recomend to Sell :%0a".$namaCoin."%0aHigh 24H : Rp ".number_format($high24H)."%0aSell Price : Rp ". number_format($sellPrice);
+            $msgRecomendtoSell = "Recomend to Sell :%0a".$namaCoin."%0aLast Price : Rp ".number_format($lastPrice)."%0aHigh 24H : Rp ".number_format($high24H)."%0aSell Price : Rp ". number_format($sellPrice);
             $api = "https://api.telegram.org/bot5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo/sendmessage?chat_id=$arrayChatId[$i]&text=$msgRecomendtoSell";
             $konten = file_get_contents($api);
             // $data = json_decode($konten, true);
@@ -37,23 +35,24 @@
         $stringKosong = "";
         $batasAmanSell = $value['high'] * 1/100;
         $batasAmanSell = $batasAmanSell;
-        if (($value['high'] - $value['last']) < $batasAmanSell) {
+        if ($value['last'] != 0 && ($value['high'] - $value['last']) < $batasAmanSell) {
             sendRecomendtoSell($stringKosong,$row,$value['last'],$value['high'],$value['sell']);
         }
+        
         // if ($value['high'] - $value['last'] == 0) {
         //     sendRecomendtoSell($stringKosong,$row,$value['last'],$value['high'],$value['sell']);
         // }
         
     }
-    foreach ($data as $row => $value) {
-        $stringKosong = "";
-        $batasAmanBuy = $value['low'] * 1/100;
-        $batasAmanBuy = $batasAmanBuy;
-        if (($value['last'] - $value['low']) < $batasAmanBuy) {
-            SendRecomendtoBuy($stringKosong,$row,$value['last'],$value['low'],$value['buy']);
-        }
-        // if ($value['last'] - $value['low'] == 0) {
-        //     SendRecomendtoBuy($stringKosong,$row,$value['last'],$value['low'],$value['buy']);
-        // }
-    }
+    // foreach ($data as $row => $value) {
+    //     $stringKosong = "";
+    //     $batasAmanBuy = $value['low'] * 1/100;
+    //     $batasAmanBuy = $batasAmanBuy;
+    //     if ($value['last'] != 0 && ($value['last'] - $value['low']) < $batasAmanBuy) {
+    //         SendRecomendtoBuy($stringKosong,$row,$value['last'],$value['low'],$value['buy']);
+    //     }
+    //     // else if ($value['last'] - $value['low'] == 0) {
+    //     //     SendRecomendtoBuy($stringKosong,$row,$value['last'],$value['low'],$value['buy']);
+    //     // }
+    // }
 ?>
