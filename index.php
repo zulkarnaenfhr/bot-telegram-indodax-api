d772b02dba95fec5a3c129cc0466fecfa1ac7ca1
Fahri Izuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2021-12-10 18:26:37 +0700

    final insya allah

--- index.php
+++ index.php
@@ -7,18 +7,6 @@
 
 <?php 
     $arrayChatId = ['1062055729','1536466209','1087365447'];
-    // $arrayChatId = ['1062055729'];
-
-    // function sendRecomendtoBuy($msgRecomendtoBuy,$namaCoin,$lastPrice,$low24H,$buyPrice){
-    //     // tinggal tambahin for
-    //     global $arrayChatId;
-    //     for ($i=0; $i < count($arrayChatId); $i++) { 
-    //         $msgRecomendtoBuy = "Recomend to Buy :%0a".$namaCoin."%0aLast Price : Rp ".number_format($lastPrice)."%0aLow 24H : Rp ".number_format($low24H)."%0aBuy Price : Rp ".number_format($buyPrice);
-    //         $api = "https://api.telegram.org/bot5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo/sendmessage?chat_id=$arrayChatId[$i]&text=$msgRecomendtoBuy";
-    //         $konten = file_get_contents($api);
-    //         // $data = json_decode($konten, true);
-    //     }
-    // }
 
     function sendRecomendtoBuy($msgSendtoBuy){
         global $arrayChatId;
@@ -72,16 +60,6 @@
         sendRecomendtoBuy($msg7);
         sendRecomendtoBuy($msg8);
     }
-
-    // function sendRecomendtoSell($msgRecomendtoSell,$namaCoin,$lastPrice,$high24H,$sellPrice){
-    //     global $arrayChatId;
-    //     for ($i=0; $i < count($arrayChatId) ; $i++) { 
-    //         $msgRecomendtoSell = "Recomend to Sell :%0a".$namaCoin."%0aLast Price : Rp ".number_format($lastPrice)."%0aHigh 24H : Rp ".number_format($high24H)."%0aSell Price : Rp ". number_format($sellPrice);
-    //         $api = "https://api.telegram.org/bot5073614406:AAEf7-M6-82p0WgCvS6RgzdMwiuorzMZnDo/sendmessage?chat_id=$arrayChatId[$i]&text=$msgRecomendtoSell";
-    //         $konten = file_get_contents($api);
-    //         // $data = json_decode($konten, true);
-    //     }
-    // }
 
     function sendRecomendtoSell($msgSendtoSell){
         global $arrayChatId;
@@ -143,12 +121,11 @@
     foreach ($data as $row => $value) {
         $msgRecomendtoSell = "";
         $batasAmanSell = $value['high'] * 1/100;
-        $batasAmanSell = $batasAmanSell;
+
         if ($value['last'] > 1 && ($value['high'] - $value['last']) < $batasAmanSell) {
-            $msgRecomendtoSell = "Recomend to Sell :%0a".$row."%0aLast Price : Rp ".number_format($value['last'])."%0aHigh 24H : Rp ".number_format($value['high'])."%0aSell Price : Rp ". number_format($value['sell'])."%0a%0a";
+            $msgRecomendtoSell = "Recomend to Sell :%0a".$row."%0aLast Price : ".number_format($value['last'])."%0aHigh 24H : ".number_format($value['high'])."%0aSell Price : ". number_format($value['sell'])."%0a%0a";
             array_push($arrayDataSell,$msgRecomendtoSell);
         }
-        
     }
     splitRecomendtoSell();
     // echo count($arrayDataSell)." ";
@@ -157,10 +134,9 @@
     foreach ($data as $row => $value) {
         $msgRecomendtoBuy = "";
         $batasAmanBuy = $value['low'] * 1/100;
-        $batasAmanBuy = $batasAmanBuy;
 
         if ($value['last'] > 1 && ($value['last'] - $value['low']) < $batasAmanBuy) {
-            $msgRecomendtoBuy = "Recomend to Buy :%0a".$row."%0aLast Price : Rp ".number_format($value['last'])."%0aLow 24H : Rp ".number_format($value['low'])."%0aBuy Price : Rp ".number_format($value['buy'])."%0a%0a";
+            $msgRecomendtoBuy = "Recomend to Buy :%0a".$row."%0aLast Price : ".number_format($value['last'])."%0aLow 24H : ".number_format($value['low'])."%0aBuy Price : ".number_format($value['buy'])."%0a%0a";
             array_push($arrayDataBuy,$msgRecomendtoBuy);
         }
     }
